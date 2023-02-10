4da95cfb679052a84e1bdbccc40a4026871e4f9f
AnejaPirnat <aneja.pirnat@scv.si>
2023-02-10 18:06:43 +0100

    added categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
     has_many :comments
     scope :published, -> { where(published_at: nil).or(where("published_at <= ?", DateTime.now)) }
     has_rich_text :body
+    belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_05_080612) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_10_165905) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_080612) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.bigint "user_id", null: false
@@ -69,6 +75,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_080612) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.datetime "published_at"
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -89,5 +97,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_080612) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "posts", "categories"
   add_foreign_key "posts", "users"
 end
