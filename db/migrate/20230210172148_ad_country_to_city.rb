a582b825ed12fb2fa45ad11ef2a6352c3e9891a7
AnejaPirnat <aneja.pirnat@scv.si>
2023-02-10 18:43:25 +0100

    name changes in navbar

--- db/migrate/20230210172148_ad_country_to_city.rb
+++
@@ -1,4 +0,0 @@
-class AdCountryToCity < ActiveRecord::Migration[7.0]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_10_172148) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_10_172814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_10_172148) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "country_id", null: false
+    t.index ["country_id"], name: "index_cities_on_country_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -110,6 +112,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_10_172148) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "cities", "countries"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "posts", "categories"
