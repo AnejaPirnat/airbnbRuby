d027ae4d85ce1c3e50850d41503f4a1e767ce62a
Aneja <91323255+AnejaPirnat@users.noreply.github.com>
2023-02-22 16:11:00 +0100

    facebook login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,23 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    def google_oauth2
+  def facebook
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    redirect_to root_path
+  end
+
+  def google_oauth2
       user = User.from_google(from_google_params)
       
-      
- 
       if user.present?
         sign_out_all_scopes
         flash[:notice] = t 'devise.omniauth_callbacks.success', kind: 'Google'
@@ -12,7 +26,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
         redirect_to new_user_session_path
       end
-    end
+  end
  
      def from_google_params
        @from_google_params ||= {
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2, :facebook]
   has_many :posts, dependent: :destroy
   has_many :comments
   has_many :reservations
   def self.from_google(u)
     create_with(uid: u[:uid], name: u[:name], provider: 'google',
                 password: Devise.friendly_token[0, 20]).find_or_create_by!(email: u[:email])
+  end
+  def self.from_omniauth(auth)
+    name_split = auth.info.name.split(" ")
+    user = User.where(email: auth.info.email).first
+    user ||= User.create!(provider: auth.provider, uid: auth.uid, last_name: name_split[0], first_name: name_split[1], email: auth.info.email, password: Devise.friendly_token[0, 20])
+      user
   end
 end
